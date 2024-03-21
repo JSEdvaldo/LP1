6df247c6a2bd512970ddf72875459c5f0e18079e
Helder Martins de Sousa <heldermartinsdesousa@gmail.com>
2024-03-21 16:59:11 -0300

    add questao 1 matriz

---
+++ Projetos C++/Matrizes/questao1.cpp
@@ -0,0 +1,42 @@
+//1.Leia uma matriz 4 x 4, conte e escreva quantos valores maiores que 10 ela possui.
+#include <iostream>
+
+using namespace std;
+
+void preencher_matriz(int matriz[4][4], int linha, int coluna){
+    for(int i = 0; i<linha; i++){
+        for (int j = 0; j<coluna; j++){
+            cout << "Digite um elemento da Matriz: ";
+            cin>> matriz[i][j];
+        }
+    }
+}
+void imprimir_matriz(int matriz[4][4],int linha, int coluna){
+    for(int i = 0; i<linha; i++){
+        for (int j = 0; j<coluna; j++){
+            cout << matriz[i][j] << " ";
+        }
+        cout<< endl;
+    }
+}
+int maior_que(int matriz[4][4],int linha, int coluna, int num){
+    int contador = 0;
+    for(int i = 0; i<linha; i++){
+        for (int j = 0; j<coluna; j++){
+            if (matriz[i][j] > num){
+                contador++;
+            }
+        }
+    }
+    return contador;
+}
+int main(){
+    const int linha = 4;
+    const int coluna = 4;
+    const int numero = 10;
+    int matriz[linha][coluna];
+
+    preencher_matriz(matriz, linha, coluna);
+    imprimir_matriz(matriz, linha, coluna);
+    cout << "O Numero de valores maiores que " << numero << " são: " << maior_que(matriz, linha, coluna, numero);
+}
